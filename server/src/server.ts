6a56011f33535e38760c5810277d2acef0af772a
Jin Bok <jin@quatredeux.com>
2023-05-19 03:55:34 -0300

    chore(server): initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  return prisma.user.findMany()
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333.')
+  })
