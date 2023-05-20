ffada8fbbc1b24f8b34aaf5567830091fb6f9532
Jin Bok <jin@quatredeux.com>
2023-05-20 06:06:02 -0300

    style(web): configure Roboto and Bai Jamjuree fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
       <h1 className="text-4xl font-bold">Your time capsule</h1>
     </div>
   )
