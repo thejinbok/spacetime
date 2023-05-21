f050c6ca5541dcf2a60ce033afc037c5b3a378a7
Jin Bok <jin@quatredeux.com>
2023-05-21 10:03:00 -0300

    style(mobile): configure Roboto and Bai Jamjuree fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className="font-bold text-zinc-50">Open up App.tsx to start working on your app!</Text>
+      <Text className="font-alt text-zinc-50">Open up App.tsx to start working on your app!</Text>
       <StatusBar style="light" translucent />
     </View>
   );
